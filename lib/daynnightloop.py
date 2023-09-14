6c8a350c5d80fd28a4b89ef2c462326d6ec17285
Nguyễn Giang <101436766+MyTaeyeon@users.noreply.github.com>
2023-09-14 20:07:27 +0700

    night and day infinity

---
+++ lib/daynnightloop.py
@@ -0,0 +1,12 @@
+import pygame
+import sys
+import utilities as u
+# import lib.utilities as u
+import numpy
+
+pygame.init()
+
+if __name__ == "__main__":
+    screen = pygame.display.set_mode([640, 320])
+    clock = pygame.time.Clock()
+    
