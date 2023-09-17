9d24fda9759ec4d281b36a3fe1a5fe38dcb3f788
Nguyễn Giang <101436766+MyTaeyeon@users.noreply.github.com>
2023-09-17 21:59:27 +0700

    code implement

--- lib/tree.py
+++ lib/tree.py
@@ -3,7 +3,10 @@ import math
 import random
 import sys
 import utilities as u
+import noise
 # import lib.utilities as u
+# import lib.noise as noise
+import threading
 
 pygame.init()
 
@@ -32,9 +35,6 @@ def drawTree(**p):
 		h2 = p['height'] * p['dheight'](dep)
 		x2 = x0+math.cos(a2)*h2
 		y2 = y0-math.sin(a2)*h2
-
-		#u.text(p['surf'],x1,y1,str(dep),(0,150,0))
-		#u.text(p['surf'],x2,y2,str(dep),(0,150,0))
 
 		u.line(p['surf'],p['color'],[x0,y0],[x1,y1],p['width'])
 		u.line(p['surf'],p['color'],[x0,y0],[x2,y2],p['width'])
@@ -71,7 +71,7 @@ def tree1(surf,x,y,shade=0):
 			 dheight = lambda dep: random.random()*0.6+0.4,
 
 			 opening = math.pi/6,
-			 dopening = lambda dep: 0.8 + random.random()*0.5,
+			 dopening = lambda dep: random.random()*0.5+0.8,
 
 			 color = (100+shade,100+shade,100+shade),
 			 depth = 0,
@@ -152,21 +152,118 @@ def tree4(surf,x,y,shade=0):
 			 maxdepth = 10
 			)
 
-if __name__ == "__main__":  
-   
-    pygame.display.set_caption("tree shape test")
-    screen = pygame.display.set_mode((640, 320))
-    screen.fill((255, 255, 255))
-    treelayer = pygame.Surface((640, 320))
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.KEYDOWN:
-                screen.fill((255, 255, 255))
-                treelayer.fill((255, 255, 255))
-                random.choice([tree1, tree2, tree3, tree4])(treelayer, 200, 320, 10)
-                # tree2(treelayer, 200, 320, 10)
-                screen.blit(treelayer, (0, 0))
-                pygame.display.flip()
+pygame.init()
+
+# setting up variables
+size = width, height = 1280, 320
+buff = 200
+screen = pygame.display.set_mode([width/2,height+50])#,pygame.FULLSCREEN )
+screen.fill((255, 255, 255))
+pygame.display.set_caption("")
+
+treeDensity = 32
+landDensity = 32
+
+allloads = width/treeDensity
+loaded = 0
+
+Ls = [None]*4
+Lrs = [None]*4
+
+gamestart = False
+
+COLOR_KEY = [255,0,255]
+
+terrain = [0]*4
+
+lspds = [0.1,0.2,0.5,1]
+totalMade = [0]*4
+
+scheme = [(70,69,63),(225,225,210)]
+
+def makeBGLayer(n):
+	global loaded, allloads, terrain, lspds, totalMade
+	print("Making Background...")
+	l = pygame.Surface([width+buff*2,height])
+	l.fill(COLOR_KEY)
+	l.set_colorkey(COLOR_KEY)
+
+	if terrain[n] == 0:
+		treesum = width/(0.0+len(Ls)*treeDensity)
+		for i in range(0,int(treesum)):
+			thetree = [ random.choice([tree2]),
+						random.choice([tree1,tree1,tree2]),
+						random.choice([tree1,tree4,tree3]),
+						random.choice([tree1,tree4,tree3]) ][n]
+			thetree(l,random.random()*width+buff,height,(120-n*30)+random.randrange(-10,10))
+			loaded += 1
+	elif terrain[n] == 1:
+
+		treesum = (width/(0.0+len(Ls)*treeDensity))
+		for i in range(0,int(math.ceil(treesum/2.0))):
+			thetree = [ random.choice([tree1,tree3]),
+						random.choice([tree1,tree3]),
+						random.choice([tree1,tree3]),
+						random.choice([tree1,tree3]) ][n]
+			thetree(l,random.random()*width+buff,height,(120-n*30)+random.randrange(-10,10))
+			loaded += 2
+		if n != 3:
+			poly = []
+			poly.append([0,height])
+			for i in range(buff,width+buff,landDensity):
+				poly.append([i,height-makeLand(i*0.05,n*0.5,500-n*90)])
+			poly[1][1] = (poly[1][1]-height)/2.0+height
+			poly[-1][1] = (poly[-1][1]-height)/2.0+height
+			poly.append([width+buff*2,height])
+			pygame.draw.polygon(l,(210-n*20,210-n*20,210-n*20),poly)
+
+	totalMade[n] += 1
+	if totalMade[n]%int(lspds[n]*10) == 0:
+		terrain[n] = (terrain[n]+1) % 2
+
+
+	print(str(loaded)+"/"+str(allloads))
+	return l
+
+
+def mt(LN, *args):
+	global Ls, Lrs, loaded, allloads
+	allloads = len(args)*(width/(len(Ls)*treeDensity))
+	loaded = 0
+	if LN == 1:
+		for a in args:
+			Ls[a] = makeBGLayer(a)
+	elif LN == 2:
+		for a in args:
+			Lrs[a] = makeBGLayer(a)
+
+def makeLand(n,m=0,maxheight = 20):
+	return max(noise.noise(n*0.1,m*0.5)*maxheight,2)-2
+
+thread1 = threading.Thread(target=mt, args=(2,  3, 2, 1, 0))
+thread1.start()
+thread1.join()
+
+while loaded<allloads:
+    pass
+
+scroll = 0
+
+while (True):
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			sys.exit()
+
+	screen.fill((255, 255, 255))
+
+	screen.blit(Lrs[0], (scroll, 0))
+	screen.blit(Lrs[1], (scroll, 0))
+	screen.blit(Lrs[2], (scroll, 0))
+	screen.blit(Lrs[3], (scroll, 0))
+
+	if pygame.key.get_pressed()[pygame.K_RIGHT]:
+		scroll -= 1
+	if pygame.key.get_pressed()[pygame.K_LEFT]:
+		scroll += 1
+	pygame.display.update()
+		
